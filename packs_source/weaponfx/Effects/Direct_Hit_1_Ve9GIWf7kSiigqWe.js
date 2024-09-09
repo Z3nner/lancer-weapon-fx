837fc45838b8314e3aea52834439bd99a35dd24a
tw-blob <74862721+tw-blob@users.noreply.github.com>
2024-09-09 18:31:18 +0200

    Update Direct_Hit_1_Ve9GIWf7kSiigqWe.js

--- packs_source/weaponfx/Effects/Direct_Hit_1_Ve9GIWf7kSiigqWe.js
+++ packs_source/weaponfx/Effects/Direct_Hit_1_Ve9GIWf7kSiigqWe.js
@@ -3,9 +3,9 @@ const {sourceToken} = game.modules.get("lancer-weapon-fx").api.getMacroVariables
 await Sequencer.Preloader.preloadForClients([
     "modules/lancer-weapon-fx/advisories/DirectHit.svg",
     "jb2a.static_electricity.03.blue",
-    "modules/JB2A_DnD5e/Library/Generic/Explosion/SideExplosion01_02_Regular_Orange_600x600.webm",
+    "jb2a.explosion_side.01.orange.1",
     "modules/lancer-weapon-fx/soundfx/DirectHitExplosion1.ogg",
-    "modules/JB2A_DnD5e/Library/Generic/Impact/Impact_05_Regular_Orange_400x400.webm",
+    "jb2a.impact.005.orange",
 ]);
 
 const pivotx = token.document.flags["hex-size-support"]?.pivotx || 0;
@@ -34,7 +34,7 @@ new Sequence()
         .repeats(2, 2600)
         .mask(sourceToken)
     .effect()
-        .file("modules/JB2A_DnD5e/Library/Generic/Explosion/SideExplosion01_02_Regular_Orange_600x600.webm")
+        .file("jb2a.explosion_side.01.orange.1")
         .atLocation(sourceToken, { randomOffset: 0.5, gridUnits: true})
         .randomSpriteRotation()
         .scaleToObject(1.4)
@@ -42,7 +42,7 @@ new Sequence()
         .delay(900)
         .opacity(0.8)
     .effect()
-        .file("modules/JB2A_DnD5e/Library/Generic/Impact/Impact_05_Regular_Orange_400x400.webm")
+        .file("jb2a.impact.005.orange")
         .atLocation(sourceToken, { randomOffset: 0.5, gridUnits: true})
         .randomSpriteRotation()
         .scaleToObject(1.4)
@@ -56,7 +56,7 @@ new Sequence()
         .repeats(3, 825)
         .delay(900)
     .effect()
-        .file("modules/JB2A_DnD5e/Library/Generic/Explosion/SideExplosion01_02_Regular_Orange_600x600.webm")
+        .file("jb2a.explosion_side.01.orange.1")
         .atLocation(sourceToken, { randomOffset: 0.5, gridUnits: true})
         .randomSpriteRotation()
         .scaleToObject()
@@ -64,7 +64,7 @@ new Sequence()
         .delay(2400)
         .opacity(0.8)
     .effect()
-        .file("modules/JB2A_DnD5e/Library/Generic/Impact/Impact_05_Regular_Orange_400x400.webm")
+        .file("jb2a.impact.005.orange")
         .atLocation(sourceToken, { randomOffset: 0.5, gridUnits: true})
         .randomSpriteRotation()
         .scaleToObject(1.5)
@@ -93,5 +93,4 @@ new Sequence()
     .sound()
         .file("modules/lancer-weapon-fx/soundfx/DirectHitExplosion2.ogg")
         .volume(game.modules.get("lancer-weapon-fx").api.getEffectVolume(0.5))
-
  .play();
