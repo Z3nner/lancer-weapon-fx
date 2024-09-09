b626ec1d07eb9078e2f26d8542deb50d7860b770
tw-blob <74862721+tw-blob@users.noreply.github.com>
2024-09-09 18:42:45 +0200

    Update System_Trauma_EGtaSw7oQsGz2sqZ.js

--- packs_source/weaponfx/Effects/System_Trauma_EGtaSw7oQsGz2sqZ.js
+++ packs_source/weaponfx/Effects/System_Trauma_EGtaSw7oQsGz2sqZ.js
@@ -1,10 +1,10 @@
 const {sourceToken} = game.modules.get("lancer-weapon-fx").api.getMacroVariables(this);
 
 await Sequencer.Preloader.preloadForClients([
-    "modules/JB2A_DnD5e/Library/Generic/Explosion/SideExplosion01_02_Regular_Orange_600x600.webm",
+    "jb2a.explosion_side.01.orange.1",
     "modules/lancer-weapon-fx/soundfx/DirectHitExplosion1.ogg",
-    "modules/JB2A_DnD5e/Library/Generic/Impact/Impact_05_Regular_Orange_400x400.webm",
-    "modules/JB2A_DnD5e/Library/Generic/Explosion/SideFractureFlask01_01_800x400.webm",
+    "jb2a.impact.005.orange",
+    "jb2a.explosion.side_fracture.flask.01.0",
 ]);
 
 const pivotx = token.document.flags["hex-size-support"]?.pivotx || 0;
@@ -16,14 +16,14 @@ const ipivoty = -pivoty;
 new Sequence()
 
     .effect()
-        .file("modules/JB2A_DnD5e/Library/Generic/Explosion/SideExplosion01_02_Regular_Orange_600x600.webm")
+        .file("jb2a.explosion_side.01.orange.1")
         .atLocation(sourceToken, { randomOffset: 0.3, gridUnits: true})
         .randomSpriteRotation()
         .scaleToObject(1.4)
         .repeats(3, 125)
         .opacity(0.8)
     .effect()
-        .file("modules/JB2A_DnD5e/Library/Generic/Impact/Impact_05_Regular_Orange_400x400.webm")
+        .file("jb2a.impact.005.orange")
         .atLocation(sourceToken, { randomOffset: 0.3, gridUnits: true})
         .randomSpriteRotation()
         .scaleToObject(1.6)
@@ -35,7 +35,7 @@ new Sequence()
         .volume(game.modules.get("lancer-weapon-fx").api.getEffectVolume(0.5))
         .repeats(3, 125)
     .effect()
-        .file("modules/JB2A_DnD5e/Library/Generic/Explosion/SideFractureFlask01_01_800x400.webm")
+        .file("jb2a.explosion.side_fracture.flask.01.0")
         .atLocation(sourceToken, { offset: { x: ipivotx, y: ipivoty}})
         .scaleToObject(1.4)
         .randomRotation()
